92ef9d58a6ec2e4c0cde0365bca99d7dd7780416
Kevin Lin <kevinlin@Kevins-MacBook-Pro.local>
2020-07-15 23:05:55 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,134 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Startup Name Generator',
+      theme: ThemeData(
+        primaryColor: Colors.deepPurple[900],
+      ),
+      home: RandomWords(),
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  _RandomWordsState createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = Set<WordPair>();
+  final _biggerFont = TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: EdgeInsets.all(20.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider(); /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  /* 1
+  The itemBuilder callback is called once per suggested word pairing, 
+  and places each suggestion into a ListTile row. 
+  For even rows, the function adds a ListTile row for the word pairing. 
+  For odd rows, the function adds a Divider widget to visually separate the entries. 
+  Note that the divider might be difficult to see on smaller devices.
+  */
+
+  /* 2
+  Add a one-pixel-high divider widget before each row in the ListView.
+  */
+
+  /* 3
+  The expression i ~/ 2 divides i by 2 and returns an integer result. 
+  For example: 1, 2, 3, 4, 5 becomes 0, 1, 1, 2, 2. 
+  This calculates the actual number of word pairings in the ListView, 
+  minus the divider widgets.
+  */
+
+  /* 4
+  If you’ve reached the end of the available word pairings, 
+  then generate 10 more and add them to the suggestions list.
+  */
+
+  Widget _buildRow(WordPair pair) {
+    final alreadySaved = _saved.contains(pair);
+
+    return ListTile(
+        title: Text(
+          pair.asPascalCase,
+          style: _biggerFont,
+        ),
+        trailing: Icon(
+          alreadySaved ? Icons.favorite : Icons.favorite_border,
+          color: alreadySaved ? Colors.pink : null,
+        ),
+        onTap: () {
+          setState(() {
+            if (alreadySaved) {
+              _saved.remove(pair);
+            } else {
+              _saved.add(pair);
+            }
+          });
+        });
+  }
+
+  void _pushSaved() {
+    Navigator.of(context).push(
+      MaterialPageRoute<void>(
+        // NEW lines from here...
+        builder: (BuildContext context) {
+          final tiles = _saved.map(
+            (WordPair pair) {
+              return ListTile(
+                title: Text(
+                  pair.asPascalCase,
+                  style: _biggerFont,
+                ),
+              );
+            },
+          );
+          final divided = ListTile.divideTiles(
+            context: context,
+            tiles: tiles,
+          ).toList();
+
+          return Scaffold(
+            appBar: AppBar(
+              title: Text('Saved Suggestions'),
+            ),
+            body: ListView(children: divided),
+          );
+        }, // ...to here.
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Startup Name Generator'), actions: [
+        IconButton(icon: Icon(Icons.list), onPressed: _pushSaved),
+      ]),
+      body: _buildSuggestions(),
+    );
+  }
+}
